0fc134d5f74b4839acf441a364bbbc6a88dd18ba
anson <anson.ooi@beans.com.my>
2021-07-27 17:25:42 +0800

    test

---
+++ lang/bm.js
@@ -0,0 +1,4 @@
+const bmLocale ={
+  route: {
+  }
+}
---
+++ lang/en.js
@@ -0,0 +1,265 @@
+const enLocale = {   
+  route: {
+    //common
+    submit: 'Submit',
+    preview: 'Preview',
+    cancel: 'cancel',
+    systemError: 'We are currently facing a technical issue. Kindly try again later. We apologize for the inconvenience.',
+    noRecord: 'No Record',
+    unauthorize: 'Unauthorize access. Please try again',
+    reviewAndConfirm: 'Review and Confirm',
+    msgSuccessful: 'successful',
+    tryAgain: 'Try Again',
+    mobileNoError: 'Mobile Number invalid format',
+
+    //Product
+    selectMerchantProduct: 'Please select your preferred Product',
+    posTitle: 'Receive Payments. Get Paid. No Hassle.',
+    posSubTitle: 'Our all-in-one terminal is the machine for your business. Take payments from credit cards and eWallets. Its easy, quick and secure.',
+    posToolTips: 'Accept payments via credit cards, debit cards, charge cards and QR code via fixed line or wireless mobile terminal with contactless feature',
+    qrappTitle: 'Accept DuitNow QR Payments. Simple & Easy',
+    qrappSubTitle: 'Save time and quickly accept DuitNow QR payments with our mobile app. Get your business started in just a matter of clicks.',
+    qrappToolTips: 'Accept DuitNow QR payments via Hong Leong Bank\'s Merchant Mobile Application (available in both Android & iOS)',
+    newApplication: 'New Application',
+    wouldLikeTo: 'I would like to...',
+    applicationTracker: 'Application Tracker',
+    resumeApplication: 'Resume Application',
+    or: 'Or',
+
+    //indentify
+    letStarted: 'Let\'s get started, select which identify you would like to apply with',
+    brn: 'Business Registration Number (BRN)',
+    nric: 'MyKad/NRIC',
+
+    //checkin
+    welcome: 'Welcome! Let us get to know your business better',
+    registrationNo: 'PLEASE ENTER YOUR BUSINESS REGISTRATION NO.',
+    businessType: 'BUSINESS TYPE',
+    registeredName: 'REGISTERED NAME',
+
+    //display
+    welcome3: 'WELCOME,',
+    takeTenMinutes1: 'It will only take you ',
+    takeTenMinutes2: '5',
+    takeTenMinutes3: ' minutes to complete the application or you can continue the application at any other time',
+    makeSureDocumentReady: 'Before starting, please make sure you have the following documents ready at hand',
+    documentSubmit: 'Here are the list of document that need to be submited',
+    photocopyIc: 'Photocopy NRIC of AUthoised Signatories*',
+    photoOfStore: 'Photos of your store',
+    boardResolution: 'Board Resolution or Memorandum & Articles',
+    bankStatement: 'Latest Bank Statements (3 months)',
+    applicatiopStep: '5 Application Steps',
+    submission: 'Submission',
+
+    //choooseDirector
+    aboutYourCompany: 'This is what we managed to find out about your company',
+    showingResult: 'Showing result of Business Registration No.',
+    fillingThisApplication: 'Please Select who is filling this application',
+    latestDirector: 'These are the latest directors & shareholders',
+    location: 'LOCATION',
+
+    //otp
+    howToGetInTouch: 'Let us  know how we can get in touch with you',
+    reachOutToYou: 'We will reach  out to your if we need further details on the application',
+    yourNric: 'YOUR NRIC NO.',
+    yourName: 'YOUR NAME',
+    emailAddress: 'WHAT IS YOUR EMAIL ADDRESS?',
+    yourMobileNo: 'ENTER YOUR MOBILE NO.',
+    enterOtp: 'ENTER 6 DIGIT OTP',
+    didntReceive: 'Did not receive OTP?',
+    resendAgain: 'Resend OTP after',
+    resend: 'Resend',
+    second: 'seconds',
+    receiveOtp: 'You will receive a One-Time Password (OTP) on your registered mobile number.',
+
+    //verification
+    icVerification: 'NRIC Verification Info',
+    name: 'NAME',
+    address: 'ADDRESS',
+    nricNo: 'NRIC NUMBER',
+    postCode: 'POST CODE',
+    city: 'CITY',
+    state: 'STATE',
+
+    //alternate
+    wrongMobileNo: 'It seems that the mobile number you entered was incorrect',
+    alternateVerification: 'Dont\' worry, we still can  verify you based on your company\'s information. Please fill in the details below',
+    paidUpCapitalTooltip: 'Please enter 0 amount if don\'t have paid up capital',
+    
+    //button
+    nextButton: 'NEXT',
+    backButton: 'BACK',
+    continueButton: 'CONTINUE',
+    getStarted: 'GET STARTED',
+    verify: 'VERIFY',
+    searchButton: 'SEARCH',
+     update: 'UPDATE',
+    updateContact: 'UPDATE CONTACT',
+
+    pleaseSelect: 'Please Select Option',
+    registrationNoPlaceholder: 'Ex. 12345678 000',
+    registeredNamePlaceholder: 'Enter registered Name',
+
+    //company
+    companyInfo: 'Company Information',
+    knowYouBetter: 'We want to get to know you better.',
+    reviewBusinessDetails: 'Please review all of your business details below',
+    businessInfo: 'Registerd Business Information',
+    businessRegNo: 'Business Registration Number',
+    businessRegName: 'Business Registration Name',
+    tradingName: 'Trading Name',
+    registeredAddress: 'Registered Address',
+    address1: 'Address 1',
+    address2: 'Address 2',
+    address3: 'Address 3',
+    sameAsRegisteredAddress: 'Same As Registered Address',
+    tradingAddress: 'Trading Address',
+    correspondenceAddress: 'Correspondence Address',
+    contactPerson: 'Contact Person',
+    contactPersonName: 'Contact Person Name',
+    contactPersonNo: 'MOBILE NUMBER',
+    contactPersonEmail: 'Contact Person Email',
+    //trading
+    aboutYourStore: 'Let\'s find out more about your store',
+    goodsAndServices: 'Type of Goods & Services      ',
+    goodsAndServicesToolTips: 'Cash & Carry refer to business whereby the goods/services are delivered at the point of payment. Non-cash & carry refer to goods/services have delayed delivery i.e spa package',
+    natureOfBusiness: 'Nature of Business',
+    projectedCardSales: 'Projected Card Sales',
+    productPriceRange: 'Product Price Range',
+    tradingLocation: 'Trading Location',
+    premiseSize: 'Premise Size',
+
+    //settlement
+    settlementInfo: 'Settlement Information',
+    chooseAbankAccount: 'Please Choose a bank account for us to transfer  the money to.',
+    referAsAccount: 'We will refer to this as the settlement account',
+    selectBankName: 'SELECT BANK NAME',
+    bankAccNo: 'BANK ACCOUNT NUMBER',
+    emailForStatement: 'E-MAIL FOR STATEMENT PURPOSES',
+
+    //authorized Signatory
+    authorizedSignatory: 'Authorized Signatory',
+    lsitOfDirector: 'List of Registered Director and Authorise Person',
+    addDirector: 'Add New Signatory',
+    directorName: 'NAME',
+    directorNric: 'NRIC',
+    directorContactNo: 'CONTACT NO.',
+    directorDesignation: 'DESIGNATION',
+    authorizedSignatory1: 'AUTHORIZED SIGNATORY',
+    updateDetails: 'Update Details',
+    verifyDetails: 'Please verify your details',
+    newAuthorizedSignatory: 'New Authorized Signatory',
+    makeAuthoriser: 'MAKE AN AUTHORISER',
+    addAuthoriser: 'ADD AUTHORISER',
+    addContactDetails: 'Add Contact Details',
+    emailAddress: 'EMAIl ADDRESS',
+
+    //PCI
+    acquirerInfo: 'Acquirer Information (PCI-DSS)',
+    needSomeInformation: 'We need a little bit of information from you in order for us to help you get your terminal all set up',
+    pciDssToolTips: 'The Payment Card Industry Data Security Standard(PCI DSS) is a set of requirements intended to ensure that all companies that process, store, or transmit credit card information maintain a secure environment. For more Information, kindly visit https://www.prisesecuritystandards.org/',
+    usedMerchantFacilities: 'HAVE YOU USED MERCHANT FACILITIES FROM OTHER BANK OR COMPANIES?',
+    selectAcquirer: 'SELECT ACQUIRER',
+    endDateOfService: 'END DATE OF SERVICE',
+    sensitiveData: 'Do you store any sensitive customer data i.e Card Number, CVV or Expiry Date?',
+    storageType: 'Type of Storage',
+    terminalConnected: 'Will your terminal connectedto your system/network?',
+    privateSegmentedNetwork: 'Is it a private/segmented network?',
+    otherApplication: 'Do you use other payment application?',
+    softwareName: 'Name of Vendor',
+    softwareVersion: 'Name & Version of Software',
+    softwareValidate: 'Has this software been validated against PADSS?',
+
+    //Pricing
+    pricingInfo: 'Pricing & T&C Aceptance',
+    fees: 'Here Are the fees (MDR) and Assosiated charges',
+    rentFee: 'Rent free for the subsequent years as long as you hit a monthly sales volume of RM60k',
+    rentFree: 'Rent Free Fee on your first year and more years',
+    cards: 'Cards',
+    eWallet: 'Ewallet',
+    ibgFee: 'IBG Fee:',
+    oneRinggit: 'RM1.00',
+    nonHLB: '(only applicable for non-HLB account)',
+    rental: 'Rental',
+    fortyRinggit: 'RM40',
+    subsequentWaiver: 'per month. Waived for Fist Year. Subsequent waiver subject to a monthly sales volume of',
+    sixtyK: 'RM60K',
+    tickingBox: 'By selecting this checkbox you are deemed to have read and agreed on the following ',
+    termAndCondition: 'Merchant Terms & Conditions',
+    declaration: 'Merchant Declaration',
+    pricingWarning1: 'All settlement will be credited to Merchants T+1 excepts for WeChat China which will credited on T+2 basic.',
+    pricingWarning2: 'This settlement conmitments is excluding weekends and Public Hodidays.',
+
+    //Upload
+    uploadInfo: 'Upload Requirer Information',
+    businessRelatedDocument: 'Please Upload your business related documents for verification',
+    boardResolutionCopy: 'CERTIFIED TRUE COPY BOARD RESOLUTION OR MEMORANDUM AND ARTICLES OF ASSOCIATION (M&A)',
+    threeMonthBankStatement: 'COPY OF LATEST 3 MONTH BANK STATEMENT WITH COMPANY STAMP/CHOP',
+    myKadCopy: 'COPY OF SIGNATOR(S) MYKAD/NRIC WITH COMPANY STAMP/CHOP',
+    uploadFile: 'Upload FIle',
+    shareSomeImage: 'Please share with us some images of your business premise',
+    frontageOfPremise: 'FRONTAGE OF PREMISEWITH SIGNAGE CLEARLY VISIBLE',
+    interiorOfPremise: 'INTERIOR OF PREMISE PREFERABLY WITH MERCHANDISE/PRODUCTS VISIBLE',
+    surroundingPremiseImage: 'SURROUNDING PREMISE IMAGE ("STREETVIEW")',
+    fileNameExceed50: 'File name exceeded 50 characters',
+    fileSizeLimitExceed: 'File size exceeded ',
+    
+    //success
+    successfullySubmitted: 'Your application has been submitted',
+    receiveEmail: 'We have sent you an email and is currently being processing the application and will reach out to you if we need further details',
+    trackApplication: 'You will receive an email confirmation. You can track your application by click on this link',
+    trackmyapplication: 'trackmyapplication',
+    backToMainMenu: 'BACK TO MAIN MENU',
+    successfullyWaiting: 'Waiting for other signatories to authorise the application',
+    reminder: 'Here\'s a reminder who you\'ve appointed as the Authorised Signatories & Authenticators',
+    sentAnEmail: 'We sent an email link to the authorised signatories & authenticators.',
+    backToHome: 'BACK TO HOME',
+    
+    sendOtp: 'Send OTP',
+    otp: 'OTP',
+    status: 'STATUS',
+
+    basicOption: 'Option 1',
+    basicPackage: 'Basic Package',
+    basicTransactionLimit: '(RM250 per trxn & RM5000 per day limit)',
+    basicRequirement: 'No additional document requirement',
+    premiumOption: 'Option 2',
+    premiumPackage: 'Premium Package',
+    premiumTransactionLimit: '(No transaction limit)',
+    premiumRequirement: 'Take 3 photo of premise and send atleast 3 different view of the branch',
+
+    takeFiveMinutes2: '5',
+
+    lastStep: 'Last step, please fill in  the OTP sent to the selected authorized signatory\'s mobile number',
+  
+    //otherauthoriserview
+    authoriserViewTitle: 'Welcome to the Hong Leong Bank Merchant Application Form',
+    authoriserViewContent: 'As part of the online process, <strong>[applicantName]</strong> has applied for a POS. As a Director of company <strong>[registerName]</strong> , you were recognised as an authorised signatory for the company. we require you to complete an OTP verification to confirm this application.',
+  
+    //status
+    applicationId: 'APPLICATION ID',
+    businessRegistrationNo: 'BUSINESS REGISTRATION NO.',
+    submissionDate: 'SUBMISSION DATE',
+
+    //trackLogin
+    checkApplication: 'Check My Application Status',
+
+    //emailLogin
+    welcome2: 'Welcome to the Hong Leong Bank Merchant Application Form',
+    reviewAndConfirm: 'REVIEW AND CONFIRM',
+    emailLoginMessage1: 'As part of the online process',
+    emailLoginMessage2: 'has applied for a',
+    emailLoginMessage3: '. As a Director of company',
+    emailLoginMessage4: 'you were recognised as an authorized signatory for the company. We require you to complete an OTP verification to confirm this applicaion.',
+  
+    //login
+    productType: 'PRODUCT TYPE',
+    brnOrNric: 'BUSINESS REGISTRATION NO./NRIC',
+    helpUsRetrieve: 'Help us retrieve  your application by filling in the details submitted in your application',
+
+    //details
+    readAllDetails: 'Kindly, Read all the details and click on confirm proceed with the application',
+    emailAgree: 'An email has been sent to respective authorized signatories(s).',
+    applicationWillProceed: 'The application will be proceed upon their endorsement'
+  }
+}
